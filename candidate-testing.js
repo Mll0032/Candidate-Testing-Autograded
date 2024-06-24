3e5d817cf3b239c203b3c92f0a0891c08eff99cc
mll0032 <mllawson18@gmail.com>
2024-06-23 19:39:43 -0500

    Part 2 Adding arrays for questions/asnwers and updating grade quiz

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,3 +1,4 @@
+//node candidate-testing.js
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
@@ -12,9 +13,15 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions =
+  ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 'What is the minimum crew size for the ISS? ' ]
+;
+ 
+let correctAnswers =
+['Sally Ride', 'true', '40', 'Trajectory', '3']
+;
+
+let candidateAnswers = []; 
 
 
 function askForName() {
@@ -26,16 +33,18 @@ candidateName = input.question("Enter name: ")
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question) 
-
+for (let i = 0; i < 5; i++) {
+  candidateAnswer = input.question(questions[i]);
+  candidateAnswers.push(candidateAnswer);
+}
 }
 
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer) {
-  console.log("Correct!");
+if (candidateAnswers != correctAnswer) {
+  console.log(`Correct! Your answers were ${candidateAnswers} Correct answers are ${correctAnswers}`);
 } else {
   console.log("Incorrect!");
 }
@@ -55,7 +64,7 @@ function runProgram() {
    gradeQuiz(this.candidateAnswers);
 }
 
-
+runProgram();
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
   candidateName: candidateName,
