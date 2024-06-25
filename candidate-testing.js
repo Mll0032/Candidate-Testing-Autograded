c28f4e080f04e1ef22a916c72d4091fe5e7a8024
mll0032 <mllawson18@gmail.com>
2024-06-24 19:09:43 -0500

    git push to test

--- candidate-testing.js
+++ candidate-testing.js
@@ -63,7 +63,7 @@ function runProgram() {
    askQuestion();
    gradeQuiz(this.candidateAnswers);
 }
-
+//git push
 runProgram();
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
